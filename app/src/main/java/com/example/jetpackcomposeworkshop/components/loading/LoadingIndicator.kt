8c9455cb2931518277048596ce11233e16457f8d
Dmytro Berezhnyi <berezh_d@modulotech.fr>
2022-08-10 01:50:38 +0300

    changed color of LoadingIndicator.kt

--- app/src/main/java/com/example/jetpackcomposeworkshop/components/loading/LoadingIndicator.kt
+++ app/src/main/java/com/example/jetpackcomposeworkshop/components/loading/LoadingIndicator.kt
@@ -7,13 +7,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
+
 @Composable
 fun LoadingIndicator(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
     ) {
         CircularProgressIndicator(
-            color = Color.Yellow,
+            color = Color(0xFF6AA8DA),
             modifier = Modifier.align(Alignment.Center)
         )
     }
